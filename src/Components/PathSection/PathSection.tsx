d3768f3a3ca123cab35b89ae2f2880ba55fcac66
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-09-19 09:10:16 +0200

    created PathSection , add new photos

---
+++ src/Components/PathSection/PathSection.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import style from './PathSection.module.scss'
+import firstImage from '../../assets/PathImages/43.png'
+import secondImage from '../../assets/PathImages/68.png'
+import thirdImage from '../../assets/PathImages/b.png'
+import fourthImage from '../../assets/PathImages/70.png'
+
+const PathSection = () => {
+
+    const images = [firstImage,secondImage,thirdImage,fourthImage]
+    return (
+        <div className={style.container}>
+            <div className={style.wrapperBlocks}>
+                <div className={style.firstBlock + " " + style.first}>
+                    <p className={style.title}>FIRST <span>New Zealand</span></p>
+                    <div className={style.wrapperImages}>
+                        {images.map(image => <img src={image} alt={'path'}/>)}
+                    </div>
+                    <div className={style.wrapperInfo}>
+                        <p>Arrival in Auckland is the main gateway to New Zealand, a city that is famous for its graceful lines of the Harbor Bridge, the sparkling waters of Waitemata Harbor and the Hauraki Gulf.</p>
+                    </div>
+                </div>
+                <div className={style.firstBlock + " " + style.second}>
+                    <p className={style.title}>SECOND <span>New Islands</span></p>
+                    <div className={style.wrapperImages}>
+                        {images.map(image => <img src={image} alt={'path'}/>)}
+                    </div>
+                    <div className={style.wrapperInfo}>
+                        <p>Arrival in Auckland is the main gateway to New Zealand, a city that is famous for its graceful lines of the Harbor Bridge, the sparkling waters of Waitemata Harbor and the Hauraki Gulf.</p>
+                    </div>
+                </div>
+            </div>
+
+            <span className={style.centerLine}></span>
+            <div className={style.wrapperBlocks}>
+                <div className={style.firstBlock + " " + style.third}>
+                    <p className={style.title}>THIRD <span>Feral Cats</span></p>
+                    <div className={style.wrapperImages}>
+                        {images.map(image => <img src={image} alt={'path'}/>)}
+                    </div>
+                    <div className={style.wrapperInfo}>
+                        <p>Arrival in Auckland is the main gateway to New Zealand, a city that is famous for its graceful lines of the Harbor Bridge, the sparkling waters of Waitemata Harbor and the Hauraki Gulf.</p>
+                    </div>
+                </div>
+                <div className={style.firstBlock + " " + style.fourth}>
+                    <p className={style.title}>FOURTH <span>Flight of the Eagle</span></p>
+                    <div className={style.wrapperImages}>
+                        {images.map(image => <img src={image} alt={'path'}/>)}
+                    </div>
+                    <div className={style.wrapperInfo}>
+                        <p>Arrival in Auckland is the main gateway to New Zealand, a city that is famous for its graceful lines of the Harbor Bridge, the sparkling waters of Waitemata Harbor and the Hauraki Gulf.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default PathSection;
--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -4,14 +4,15 @@ import HomeContent from "./HomeContent/HomeContent";
 import style from './HomePage.module.scss'
 import bg from '../../assets/2bgg.png'
 import PhotosSection from "../../Components/PhotosSection/PhotosSection";
+import PathSection from "../../Components/PathSection/PathSection";
+
 const HomePage = () => {
     return (
-        <div className={style.container} style={{backgroundImage:`url(${bg})`}}>
+        <div className={style.container} style={{backgroundImage: `url(${bg})`}}>
             <Logo></Logo>
             <HomeContent></HomeContent>
-
-                <PhotosSection></PhotosSection>
-
+            <PathSection></PathSection>
+            <PhotosSection></PhotosSection>
 
 
         </div>
