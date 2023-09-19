29cf84e8d4ad407394119cbb28805105c90fe15a
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-09-19 09:26:53 +0200

    changed style for PathSection

--- src/Components/PathSection/PathSection.tsx
+++ src/Components/PathSection/PathSection.tsx
@@ -7,53 +7,66 @@ import fourthImage from '../../assets/PathImages/70.png'
 
 const PathSection = () => {
 
-    const images = [firstImage,secondImage,thirdImage,fourthImage]
+    const images = [firstImage, secondImage, thirdImage, fourthImage]
     return (
-        <div className={style.container}>
-            <div className={style.wrapperBlocks}>
-                <div className={style.firstBlock + " " + style.first}>
-                    <p className={style.title}>FIRST <span>New Zealand</span></p>
-                    <div className={style.wrapperImages}>
-                        {images.map(image => <img src={image} alt={'path'}/>)}
+        <>
+            <header>
+                <h1><span>Tour </span>itinerary</h1>
+            </header>
+            <div className={style.container}>
+                <div className={style.wrapperBlocks}>
+                    <div className={style.firstBlock + " " + style.first}>
+                        <p className={style.title}>FIRST <span>New Zealand</span></p>
+                        <div className={style.wrapperImages}>
+                            {images.map(image => <img src={image} alt={'path'}/>)}
+                        </div>
+                        <div className={style.wrapperInfo}>
+                            <p>Arrival in Auckland is the main gateway to New Zealand, a city that is famous for its
+                                graceful lines of the Harbor Bridge, the sparkling waters of Waitemata Harbor and the
+                                Hauraki Gulf.</p>
+                        </div>
                     </div>
-                    <div className={style.wrapperInfo}>
-                        <p>Arrival in Auckland is the main gateway to New Zealand, a city that is famous for its graceful lines of the Harbor Bridge, the sparkling waters of Waitemata Harbor and the Hauraki Gulf.</p>
+                    <div className={style.firstBlock + " " + style.second}>
+                        <p className={style.title}>SECOND <span>New Islands</span></p>
+                        <div className={style.wrapperImages}>
+                            {images.map(image => <img src={image} alt={'path'}/>)}
+                        </div>
+                        <div className={style.wrapperInfo}>
+                            <p>Arrival in Auckland is the main gateway to New Zealand, a city that is famous for its
+                                graceful lines of the Harbor Bridge, the sparkling waters of Waitemata Harbor and the
+                                Hauraki Gulf.</p>
+                        </div>
                     </div>
                 </div>
-                <div className={style.firstBlock + " " + style.second}>
-                    <p className={style.title}>SECOND <span>New Islands</span></p>
-                    <div className={style.wrapperImages}>
-                        {images.map(image => <img src={image} alt={'path'}/>)}
-                    </div>
-                    <div className={style.wrapperInfo}>
-                        <p>Arrival in Auckland is the main gateway to New Zealand, a city that is famous for its graceful lines of the Harbor Bridge, the sparkling waters of Waitemata Harbor and the Hauraki Gulf.</p>
-                    </div>
-                </div>
-            </div>
 
-            <span className={style.centerLine}></span>
-            <div className={style.wrapperBlocks}>
-                <div className={style.firstBlock + " " + style.third}>
-                    <p className={style.title}>THIRD <span>Feral Cats</span></p>
-                    <div className={style.wrapperImages}>
-                        {images.map(image => <img src={image} alt={'path'}/>)}
+                <span className={style.centerLine}></span>
+                <div className={style.wrapperBlocks}>
+                    <div className={style.firstBlock + " " + style.third}>
+                        <p className={style.title}>THIRD <span>Feral Cats</span></p>
+                        <div className={style.wrapperImages}>
+                            {images.map(image => <img src={image} alt={'path'}/>)}
+                        </div>
+                        <div className={style.wrapperInfo}>
+                            <p>Arrival in Auckland is the main gateway to New Zealand, a city that is famous for its
+                                graceful lines of the Harbor Bridge, the sparkling waters of Waitemata Harbor and the
+                                Hauraki Gulf.</p>
+                        </div>
                     </div>
-                    <div className={style.wrapperInfo}>
-                        <p>Arrival in Auckland is the main gateway to New Zealand, a city that is famous for its graceful lines of the Harbor Bridge, the sparkling waters of Waitemata Harbor and the Hauraki Gulf.</p>
+                    <div className={style.firstBlock + " " + style.fourth}>
+                        <p className={style.title}>FOURTH <span>Flight of the Eagle</span></p>
+                        <div className={style.wrapperImages}>
+                            {images.map(image => <img src={image} alt={'path'}/>)}
+                        </div>
+                        <div className={style.wrapperInfo}>
+                            <p>Arrival in Auckland is the main gateway to New Zealand, a city that is famous for its
+                                graceful lines of the Harbor Bridge, the sparkling waters of Waitemata Harbor and the
+                                Hauraki Gulf.</p>
+                        </div>
                     </div>
                 </div>
-                <div className={style.firstBlock + " " + style.fourth}>
-                    <p className={style.title}>FOURTH <span>Flight of the Eagle</span></p>
-                    <div className={style.wrapperImages}>
-                        {images.map(image => <img src={image} alt={'path'}/>)}
-                    </div>
-                    <div className={style.wrapperInfo}>
-                        <p>Arrival in Auckland is the main gateway to New Zealand, a city that is famous for its graceful lines of the Harbor Bridge, the sparkling waters of Waitemata Harbor and the Hauraki Gulf.</p>
-                    </div>
-                </div>
-            </div>
 
-        </div>
+            </div>
+        </>
     );
 };
 
